954b5becee44150ffd01002bd034278fa13116a7
matveevrj <112893541+matveevrj@users.noreply.github.com>
2022-09-08 21:03:08 +0300

    Update and rename WB-MR6Cv3-gate-controlling.js to wb-mr6c-gate-controlling.js
    Заменил модуль реле

--- WM-MR6Cv3-gate-controlling/wb-mr6c-gate-controlling.js
+++ WM-MR6Cv3-gate-controlling/wb-mr6c-gate-controlling.js
@@ -59,32 +59,32 @@ defineVirtualDevice("GateControlling", {
 
 //Открытие ворот
 defineRule("GateStartOpen", {                
-  whenChanged: ["GateControlling/GateOpen", "wb-mr6cv3_135/Input 5 counter"],
+  whenChanged: ["GateControlling/GateOpen", "wb-mr6c_135/Input 5 counter"],
   then: function () {
-  if(dev["wb-mr6cv3_135/Input 0"] == false && dev["GateControlling/Foto"] == false) { 
-    	dev["wb-mr6cv3_135/K1"] = true;
-        dev["wb-mr6cv3_135/K2"] = false;
+  if(dev["wb-mr6c_135/Input 0"] == false && dev["GateControlling/Foto"] == false) { 
+    	dev["wb-mr6c_135/K1"] = true;
+        dev["wb-mr6c_135/K2"] = false;
     }   
   }
 });
 
 //Закрытие ворот
 defineRule("GateStartClose", {               
-  whenChanged: ["GateControlling/GateClose", "wb-mr6cv3_135/Input 4 counter"],
+  whenChanged: ["GateControlling/GateClose", "wb-mr6c_135/Input 4 counter"],
   then: function () {   
-     if(dev["wb-mr6cv3_135/Input 1"] == false && dev["GateControlling/Foto"] == false) { 
-    	dev["wb-mr6cv3_135/K2"] = true;
-        dev["wb-mr6cv3_135/K1"] = false;
+     if(dev["wb-mr6c_135/Input 1"] == false && dev["GateControlling/Foto"] == false) { 
+    	dev["wb-mr6c_135/K2"] = true;
+        dev["wb-mr6c_135/K1"] = false;
     }     
   }
 });
 
 //Остановка ворот
 defineRule("GateStartStop", {   
-  whenChanged: ["GateControlling/GateStop", "wb-mr6cv3_135/Input 2 counter", "wb-mr6cv3_135/Input 6 counter"],
+  whenChanged: ["GateControlling/GateStop", "wb-mr6c_135/Input 2 counter", "wb-mr6c_135/Input 6 counter"],
   then: function () {   
-  dev["wb-mr6cv3_135/K1"] = false;
-  dev["wb-mr6cv3_135/K2"] = false;   
+  dev["wb-mr6c_135/K1"] = false;
+  dev["wb-mr6c_135/K2"] = false;   
   }
 });
 
@@ -92,30 +92,30 @@ defineRule("GateStartStop", {
 defineRule("DoorOpen", {   
   whenChanged: "GateControlling/DoorOpen",
   then: function () {   
-  dev["wb-mr6cv3_135/K3"] = true;
+  dev["wb-mr6c_135/K3"] = true;
   setTimeout(function () {
-       dev["wb-mr6cv3_135/K3"] = false;
+       dev["wb-mr6c_135/K3"] = false;
     }, 1000)   
   }
 });
 
 //Отслеживание положения ворот 
 defineRule("Position", {   
-  whenChanged: ["wb-mr6cv3_135/Input 0", "wb-mr6cv3_135/Input 1", "wb-mr6cv3_135/Input 2", "wb-mr6cv3_135/Input 3"],
+  whenChanged: ["wb-mr6c_135/Input 0", "wb-mr6c_135/Input 1", "wb-mr6c_135/Input 2", "wb-mr6c_135/Input 3"],
   then: function () {   
- dev["GateControlling/isOpen"] = dev["wb-mr6cv3_135/Input 0"];  //Датчик Открыто
- dev["GateControlling/isClosed"] = dev["wb-mr6cv3_135/Input 1"];   //Датчик Закрыто
- dev["GateControlling/Foto"] = dev["wb-mr6cv3_135/Input 2"];   //Фотоэлемет
- dev["GateControlling/Call"] = dev["wb-mr6cv3_135/Input 3"];   //Звонок
+ dev["GateControlling/isOpen"] = dev["wb-mr6c_135/Input 0"];  //Датчик Открыто
+ dev["GateControlling/isClosed"] = dev["wb-mr6c_135/Input 1"];   //Датчик Закрыто
+ dev["GateControlling/Foto"] = dev["wb-mr6c_135/Input 2"];   //Фотоэлемет
+ dev["GateControlling/Call"] = dev["wb-mr6c_135/Input 3"];   //Звонок
     
- if(dev["wb-mr6cv3_135/Input 0"] == true) dev["wb-mr6cv3_135/K1"] = false; 
- if(dev["wb-mr6cv3_135/Input 1"] == true) dev["wb-mr6cv3_135/K2"] = false;
+ if(dev["wb-mr6c_135/Input 0"] == true) dev["wb-mr6c_135/K1"] = false; 
+ if(dev["wb-mr6c_135/Input 1"] == true) dev["wb-mr6c_135/K2"] = false;
   
- if(dev["wb-mr6cv3_135/Input 2"] == true) { 
-    dev["wb-mr6cv3_135/K1"] = false;
-    dev["wb-mr6cv3_135/K2"] = false;
+ if(dev["wb-mr6c_135/Input 2"] == true) { 
+    dev["wb-mr6c_135/K1"] = false;
+    dev["wb-mr6c_135/K2"] = false;
        } 
     
-    dev["GateControlling/Alarm"] = dev["wb-mr6cv3_135/Input 2"];
+    dev["GateControlling/Alarm"] = dev["wb-mr6c_135/Input 2"];
   }
 });
