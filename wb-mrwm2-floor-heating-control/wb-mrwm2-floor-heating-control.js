7f660766c9cd6e89932ede04df7f85af0e6e6695
matveevrj <112893541+matveevrj@users.noreply.github.com>
2022-11-03 19:06:05 +0300

    Update wb-mrwm2-floor-heating-control.js

--- wb-mrwm2-floor-heating-control/wb-mrwm2-floor-heating-control.js
+++ wb-mrwm2-floor-heating-control/wb-mrwm2-floor-heating-control.js
@@ -1,135 +1,153 @@
 
 //Virtual device for floor heating control
 var Floor = { 
-OnOff1: {             //Button for switching on the primary circuit
-    type : "switch",
-    value : false,
-    readonly: false,
-    order: 1
-  },
-OnOff2: {             //Button for turning on the second circuit
-    type : "switch",
-    value : false,
-    readonly: false,
-    order: 2
-  },  
-IsOn2: {              //Primary circuit in heating mode
-    type : "switch",
-    value : false,
-    readonly: true,
-    order: 3
-  },
-IsOn2: {              //Second circuit in heating mode
-    type : "switch",
-    value : false,
-    readonly: true,
-    order: 4
-  },
-Temp1: {              //Primary heating element temperature
-    type : "value",
-    value : 0,
-    order: 5
-  },
-Temp2: {              //Second heating element temperature
-    type : "value",
-    value : 0,
-    order: 6
-  },
-Alarm1: {              //Malfunction in the primary circuit
-    type : "switch",
-    value : false,
-    readonly: false,
-    order: 7
-  },
-Alarm2: {              //Malfunction in the second circuit
-    type : "switch",
-    value : false,
-    readonly: false,
-    order: 8
-  },
-Tmax: {              //Upper heating element temperature set point
-    type : "value",
-    value : 0,
-    order: 9,
-    readonly: false    
-  },
-Tmin: {              //Lower heating element temperature set point
-    type : "value",
-    value : 0,
-    order: 10,
-    readonly: false
-  },
-Pmax1: {              //Maximum permissible power of the primary circuit 
-    type : "value",
-    value : 0,
-    order: 11,
-    readonly: false  
-  },
-Pmax2: {              //Maximum permissible power of the second circuit
-    type : "value",
-    value : 0,
-    order: 12,
-    readonly: false  
-  },   
-};
+    OnOff1: {             //Button for switching on the primary circuit
+        type : "switch",
+        value: false,
+        readonly: false,
+        order: 1,
+        title: "Первый контур"
+       },
+    OnOff2: {             //Button for turning on the second circuit
+        type : "switch",
+        value : false,
+        readonly: false,
+        order: 2
+      },  
+    IsOn2: {              //Primary circuit in heating mode
+        type : "switch",
+        value : false,
+        readonly: true,
+        order: 3
+      },
+    IsOn2: {              //Second circuit in heating mode
+        type : "switch",
+        value : false,
+        readonly: true,
+        order: 4
+      },
+    Temp1: {              //Primary heating element temperature
+        type : "value",
+        value : 0,
+        order: 5
+      },
+    Temp2: {              //Second heating element temperature
+        type : "value",
+        value : 0,
+        order: 6
+      },
+    Alarm1: {              //Malfunction in the primary circuit
+        type : "switch",
+        value : false,
+        readonly: true,
+        order: 7
+      },
+    Alarm2: {              //Malfunction in the second circuit
+        type : "switch",
+        value : false,
+        readonly: true,
+        order: 8
+      },
+    Tmax: {              //Upper heating element temperature set point
+        type : "value",
+        value : 0,
+        order: 9,
+        readonly: false    
+      },
+    Tmin: {              //Lower heating element temperature set point
+        type : "value",
+        value : 0,
+        order: 10,
+        readonly: false
+      },
+    Pmax1: {              //Maximum permissible power of the primary circuit 
+        type : "value",
+        value : 0,
+        order: 11,
+        readonly: false  
+      },
+    Pmax2: {              //Maximum permissible power of the second circuit
+        type : "value",
+        value : 0,
+        order: 12,
+        readonly: false  
+      },   
+    };
+    
+      defineVirtualDevice("Heating", {
+        title:"Floor heating",         
+        cells: Floor
+      });
+    
 
-  defineVirtualDevice("Heating", {
-    title:"Теплый пол",
-    cells: Floor
-  });
+     //Temperature sensors: "xxxxxxxx" - replace with the address of the temperature sensor, specify the correct id of the relay module  
+    var TempSensor1 = "wb-w1/xxxxxxxx";   //First temperature sensor 
+    var TempSensor2 = "wb-w2/xxxxxxxx";   //Second temperature sensor
 
-//Уставки температуры
-var Tmax = 45;   //Upper temperature limit of the heating element
-var Tmin = 35;     //Lower limit of heating element temperature
-var Pmax1 = 1000;   //Maximum permissible power of the primary circuit
-var Pmax2 = 1000;     //Maximum permissible power of the second circuit
- 
-//Primary circuit control, "xxxxxxxx" - replace with the address of the temperature sensor, specify the correct id of the relay module
-defineRule("Heating1",  {                
-  whenChanged: ["wb-w1/xxxxxxxx", "Floor/OnOff1"],
-  then: function () {
-    if(dev["wb-w1/xxxxxxxx"] < dev["Floor/Tmin"] && dev["Floor/OnOff1"] = true) { 
-    dev["wb-mrwm2_21/K1"] = true
-    };
-    if(dev["wb-w1/xxxxxxxx"] > dev["Floor/Tmax"]) { 
-    dev["wb-mrwm2_21/K1"] = false
-    };
-    dev["Floor/Temp1"] = dev["wb-w1/xxxxxxxx"];
-  }
- 
-});
+    //Chanels of relay module for heating controlling
+    var FirstCircuitControl = "wb-mrwm2_ID/K1"; //where ID is modbus adress of WB-MRWM2 module
+    var SecondCircuitControl = "wb-mrwm2_ID/K2";
+    
+    //Power meter MQTT-topics of WB-MRWM2 module
+    var FirstPowerMeter = "wb-mrwm2_ID/P L1";  //where ID is modbus adress of WB-MRWM2 module
+    var SecondPowerMeter = "wb-mrwm2_ID/P L2";  
 
-//Second circuit control, "xxxxxxxx" - replace with the address of the temperature sensor, specify the correct id of the relay module
-defineRule("Heating2",  {                
-  whenChanged: ["wb-w2/xxxxxxxx", "Floor/OnOff2"],
-  then: function () {
-    if(dev["wb-w2/xxxxxxxx"] < dev["Floor/Tmin"] && dev["Floor/OnOff2"] = true) { 
-    dev["wb-mrwm2_21/K2"] = true
-    };
-    if(dev["wb-w2/xxxxxxxx"] > dev["Floor/Tmax"]) { 
-    dev["wb-mrwm2_21/K2"] = false
-    };
-    dev["Floor/Temp2"] = dev["wb-w2/xxxxxxxx"];
-  }
-});
 
-//Fault signaling. Specify the correct id of the relay module
-defineRule("Alarm",  {                
-  whenChanged: ["wb-mrwm2_21/P L1", "wb-mrwm2_21/P L2", "Floor/OnOff1", "Floor/OnOff2"],
-  then: function () {
-    if(dev["wb-mrwm2_21/P L1"] > dev["Floor/Pmax1"]) {       //Если потребляемая мощность выше допустимой, то нагревательный элемент неисправен
-      dev["Floor/Alarm1"] = true;
-      dev["wb-mr6cv3_135/K1"] = false
-    };
-    if(dev["wb-mrwm2_21/P L2"] > dev["Floor/Pmax2"]) { 
-      dev["Floor/Alarm2"] = true;
-      dev["wb-mr6cv3_135/K2"] = false
-    };
-    if(dev["Floor/OnOff1"] = true && dev["wb-mrwm2_21/P L1"] < 1) {   //Если после включения потребляемая мощность не увеличилась, то обрыв на линии
-    dev["Floor/Alarm1"] = true
-    };
-    if(dev["Floor/OnOff2"] = true && dev["wb-mrwm2_21/P L2"] < 1) { 
-    dev["Floor/Alarm2"] = true
-    };
-  }
-});
+    //Settings
+    var Tmax = 45;   //Upper temperature limit of the heating element, 
+    var Tmin = 35;     //Lower limit of heating element temperature
+    var Pmax1 = 1000;   //Maximum permissible power of the primary circuit
+    var Pmax2 = 1000;     //Maximum permissible power of the second circuit
+     
+
+    //First circuit control
+    defineRule("Heating1",  {                
+      whenChanged: [TempSensor1, "Heating/OnOff1"],
+      then: function () {
+        if(dev[TempSensor1] < Tmin && dev["Heating/OnOff1"] == true) { 
+        dev[FirstCircuitControl] = true
+        };
+        if(dev[TempSensor1] > Tmax) { 
+        dev[FirstCircuitControl] = false
+        };
+        dev["Heating/Temp1"] = dev[TempSensor1];
+      }
+    });
+    
+    //Second circuit control
+    defineRule("Heating2",  {                
+      whenChanged: [TempSensor2, "Heating/OnOff2"],
+      then: function () {
+        if(dev[TempSensor2] < Tmin && dev["Heating/OnOff2"] == true) { 
+        dev[SecondCircuitControl] = true
+        };
+        if(dev[TempSensor2] > Tmax) { 
+        dev[SecondCircuitControl] = false
+        };
+        dev["Heating/Temp2"] = dev[TempSensor2];
+      }
+    });
+    
+    //Fault signaling
+    defineRule("Alarm",  {                
+      whenChanged: [FirstPowerMeter, SecondPowerMeter, "Heating/OnOff1", "Heating/OnOff2"],
+      then: function () {
+        if(dev[FirstPowerMeter] > Pmax1) {       //If the power consumption exceeds the limit, the heating element current is faulty
+          dev["Heating/Alarm1"] = true;
+          dev[FirstCircuitControl] = false
+        };
+        if(dev[SecondPowerMeter] > Pmax2) { 
+          dev["Heating/Alarm2"] = true;
+          dev[SecondCircuitControl] = false
+        };
+
+
+        if(dev["Heating/OnOff1"] == true && dev[FirstPowerMeter] < 1) {   //If after switching on, the power consumption has not increased, then a break in the line
+        dev["Heating/Alarm1"] = true
+        };
+        if(dev["Heating/OnOff2"] == true && dev[SecondPowerMeter] < 1) { 
+        dev["Heating/Alarm2"] = true
+        };
+      }
+    });
+    
